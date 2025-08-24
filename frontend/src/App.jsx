c38872d31354092925f4468bd800a25c34d4a137
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-08-24 23:05:06 +0530

    imported navbar and home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,13 @@
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
