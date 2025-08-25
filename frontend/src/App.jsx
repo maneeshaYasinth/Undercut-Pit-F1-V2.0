47d87dac85dc52341f3e678c2b009409036858f8
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-08-25 12:58:05 +0530

    added routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
+import Login from "./pages/Login"
+import Drivers from "./pages/Drivers"
 
 function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/drivers" element={<Drivers />} />
+      </Routes>
     </>
   );
 }
