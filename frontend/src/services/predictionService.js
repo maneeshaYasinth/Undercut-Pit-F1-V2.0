841107ad97867d5ec05074d7671df32b272aa737
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-09-01 12:50:17 +0530

    added prediction service

---
+++ frontend/src/services/predictionService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/predictions";
+
+// Submit a prediction
+export const submitPrediction = async (season, round, qualifyingWinner, raceTopThree) => {
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("Not logged in");
+
+  const res = await axios.post(
+    API_URL,
+    { season, round, qualifyingWinner, raceTopThree },
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    }
+  );
+
+  return res.data;
+};
+
+// Get user's predictions
+export const getMyPredictions = async () => {
+  const token = localStorage.getItem("token");
+  if (!token) throw new Error("Not logged in");
+
+  const res = await axios.get(`${API_URL}/mine`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  return res.data;
+};
