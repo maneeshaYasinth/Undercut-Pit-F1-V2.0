1679195f6444e38f5282820a0d5588afcafed58a
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-09-02 13:19:25 +0530

    added sevice for leaderboard

---
+++ frontend/src/services/leaderboardService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { getToken } from "./authService";
+
+const API_URL = "http://localhost:5000/api/scores/leaderboard";
+
+/**
+ * 
+ * @param {string} season 
+ * @returns {Promise<Array>} 
+ */
+export const getLeaderboard = async (season) => {
+  try {
+    const token = getToken();
+    const url = `${API_URL}?season=${season}`; // send season as query param
+    const res = await axios.get(url, {
+      headers: token ? { Authorization: `Bearer ${token}` } : {},
+    });
+    return res.data;
+  } catch (error) {
+    console.error("Error fetching leaderboard:", error);
+    throw error;
+  }
+};
