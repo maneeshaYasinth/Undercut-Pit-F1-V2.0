75152e9f30dc7ebb300671b6533b773d827c0047
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-09-03 22:15:12 +0530

    added service for news

---
+++ frontend/src/services/newsService.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:5000/api/news";
+
+export const getNews = async () => {
+  try {
+    const response = await axios.get(API_URL);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching news:", error);
+    throw error;
+  }
+};
