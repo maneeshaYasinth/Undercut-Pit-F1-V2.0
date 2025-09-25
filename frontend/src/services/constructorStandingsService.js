f22f34866468b1be63297597895a28cceac25a32
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-09-25 21:51:22 +0530

    recoded constructerstanding  with reusable link

--- frontend/src/services/constructorStandingsService.js
+++ frontend/src/services/constructorStandingsService.js
@@ -1,23 +1,17 @@
-import axios from "axios";
-import { getToken } from "./authService";
-
-const API_URL = "http://localhost:5000/api/constructorstandings";
+import api from "./api";
 
 /**
- * Fetch constructor standings for a specific season.
- * @param {string} season 
- * @returns {Promise<Array>} 
+ * Fetch driver standings for a season
+ * @param {string} season
+ * @returns {Promise<Array>}
  */
+
 export const getConstructorStandings = async (season) => {
-  try {
-    const token = getToken();
-    const url = `${API_URL}?season=${season}`;
-    const res = await axios.get(url, {
-      headers: token ? { Authorization: `Bearer ${token}` } : {},
-    });
-    return res.data;
+  try{
+    const response = await api.get(`/constructorstandings?season=${season}`);
+    return response.data;
   } catch (error) {
     console.error("Error fetching constructor standings:", error);
     throw error;
   }
-};
+}
