fe28cbbff25434e4c9abfec87b4df4e4630c1775
maneeshaYasinth <maneeshayasinthgunarathna@gmail.com>
2025-10-25 12:15:04 +0530

    upgated the routes for admin

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,18 @@
 const express = require("express");
-const { registerUser, loginUser } = require("../controllers/authController");
+const { registerUser, registerAdmin, loginUser, loginAdmin } = require("../controllers/authController");
 
 const router = express.Router();
 
 // Register
 router.post("/register", registerUser);
 
+//register as admin
+router.post("/admin/register", registerAdmin);
+
 // Login
 router.post("/login", loginUser);
+
+//login as admin
+router.post("/admin/login", loginAdmin);
 
 module.exports = router;
